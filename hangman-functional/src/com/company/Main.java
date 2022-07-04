08462639a59105bcc79cb082d979ba1f5e2f5713
JintekiWarrior <jintekiwarrior@gmail.com>
2022-07-04 16:01:37 -0400

    Add game art text file Add method to create the gameArt object Add method to concatonate a gameArtPiece

--- hangman-functional/src/com/company/Main.java
+++ hangman-functional/src/com/company/Main.java
@@ -1,8 +1,54 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        ArrayList<ArrayList <String>> gameArt = initializeGameArt();
+        System.out.println(gameArtString(gameArt.get(3)));
+    }
+
+    public static ArrayList<ArrayList <String>> initializeGameArt()
+    {
+        File file;
+        // make the art in a file
+        // read the art
+        file = new File("C:\\Users\\moeya\\Documents\\programming\\Java\\genspark-projects\\hangman-functional\\src\\resources\\hangmanArt.txt");
+        ArrayList<ArrayList <String>> outer = new ArrayList<>();
+        ArrayList<String> inner;
+        String st = "";
+        try
+        {
+            BufferedReader fr = new BufferedReader(new FileReader(file));
+            st = fr.readLine();
+            while(st != null)
+            {
+                inner = new ArrayList<>();
+                for (int i = 0; i < 5; i++)
+                {
+                    inner.add(st);
+                    st = fr.readLine();
+                }
+                outer.add(inner);
+            }
+
+        } catch (Exception e)
+        {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return outer;
+    }
+
+    // function to grab elements in the array
+    public static String gameArtString(ArrayList<String> arr)
+    {
+        return arr.stream().collect(Collectors.joining("\n"));
     }
 }
